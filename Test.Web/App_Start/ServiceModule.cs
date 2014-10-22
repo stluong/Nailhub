cf6377e48172fd353ae22679788c405ab8270ffd
Steven Thang Luong <luongthangprog@gmail.com>
2014-10-22 13:05:00 -0500

    stop here

--- Test.Web/App_Start/ServiceModule.cs
+++ Test.Web/App_Start/ServiceModule.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AppCore.Service;
+using AppService;
 using Autofac;
+using Entity;
+using Generic.Core.Service;
+using Generic.Infracstructure.Services;
 
-namespace Test.Web.App_Start
+namespace Test
 {
     public class ServiceModule: Module
     {
-        
+        protected override void Load(ContainerBuilder builder) {
+            //builder.RegisterType(typeof(Service<AspNetUser>)).As(typeof(IService<AspNetUser>)).InstancePerRequest();
+            builder.RegisterType(typeof(IdentiyService)).As(typeof(IIdentityService)).InstancePerRequest();
+        }
     }
 }
