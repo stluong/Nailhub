0f438bd78f0bd4da5506e04cd1348dd03379d426
Steven Thang Luong <luongthangprog@gmail.com>
2014-10-31 13:17:52 -0500

    Clean solution before sync with remote

--- Generic.Core/Model/Identity/ApplicationUserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Generic.Core.Model.Identity
-{
-    public class ApplicationUserRole
-    {
-        public virtual int RoleId { get; set; }
-        public virtual int UserId { get; set; }
-    }
-}
