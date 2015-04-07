e6d82cccf43038b990dc8b1f2b6bea7d9a968469
stevenluong <steven.luong@marcone.com>
2015-04-06 23:33:50 -0500

    just a commit

---
+++ Mybrus/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using AppCore.Service;
+using AppService;
+using System.Linq;
+
+
+namespace Test.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IIdentityService identity;
+
+        
+        public ProductController(IIdentityService _identity)
+        {
+            this.identity = _identity; 
+        }
+        
+        //public async Task<ActionResult> Index(int pageIndex = 1)
+        //{
+        //    //ViewBag.PageIndex = pageIndex;
+        //    //var user = await identity.Query().GetAsync();
+        //    return View();
+        //}
+        public ActionResult Index() {
+            var msg = identity.GetMessage();
+            ViewBag.Msg = string.Format("Hello {0}, {1}", identity.Query().Get(u => u.UserName).SingleOrDefault(), msg);
+            return View();
+        }
+	}
+}
